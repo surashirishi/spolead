fc980af5abe9e12e60dd8772d166d3fab8315598
satoshi <urano3104@gmail.com>
2020-06-22 12:03:54 +0900

    top-page mock

--- README.md
+++ README.md
@@ -20,3 +20,6 @@ $ yarn generate
 ```
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+<!-- lint fix -->
+./node_modules/.bin/eslint ./ --fix
---
+++ pages/top.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <div class="page-header">
+      <div class="page-header-title">
+        総合スポーツチーム口コミ情報サイト
+      </div>
+      <div class="page-header-title">
+        <v-btn large>
+          ログイン
+        </v-btn>
+      </div>
+    </div>
+    <div class="page-title">
+      SPOLEAD
+    </div>
+    <v-flex
+      xs12
+      sm8
+      md6
+      d-flex
+      flex-wrap
+      class="page-content"
+    >
+      <v-col
+        v-for="card in cards"
+        :key="card.title"
+        :cols="card.flex"
+      >
+        <v-card>
+          <v-img
+            :src="card.src"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+          >
+            <v-card-title v-text="card.title" />
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-flex>
+    <v-btn large>
+      ユーザー登録
+    </v-btn>
+  </v-layout>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    // Logo,
+    // VuetifyLogo
+  },
+  data () {
+    return {
+      cards: [
+        { title: 'Soccer', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Baseball', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Basketball', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
+        { title: 'Volleyball', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+        { title: 'Dance', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+        { title: 'Rugby', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+        { title: 'Swimming', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-header {
+  width: 100%;
+  height: 10vh;
+  margin: 16px;
+  font-size: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.page-title {
+  width: 100%;
+  height: 13vh;
+  font-size: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.page-content {
+  margin: 32px;
+}
+</style>
