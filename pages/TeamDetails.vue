09b34339dcae6c0e97eedda5eac49a6e4b7ffd45
satoshi <urano3104@gmail.com>
2020-06-24 20:14:14 +0900

    feat: team-tabs

--- pages/TeamDetails.vue
+++ pages/TeamDetails.vue
@@ -32,6 +32,7 @@
           活動エリア：東京都墨田区/ジャンル：野球
         </div>
         <v-tabs
+          v-model="tab"
           fixed-tabs
           background-color="primary"
           dark
@@ -47,42 +48,129 @@
             活動場所
           </v-tab>
         </v-tabs>
-        <div class="page-content-item-main">
-          <div class="page-content-item-list">
-            <v-card class="d-inline-block mx-auto">
-              <v-container>
-                <v-row justify="space-between">
-                  <v-col cols="auto">
-                    <v-img
-                      height="200"
-                      width="200"
-                      src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
-                    />
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card>
-          </div>
-          <div class="page-content-item-list">
-            <v-container>
-              <v-row justify="space-between">
-                <v-col cols="auto">
-                  <v-textarea
-                    :readonly="true"
-                    autocomplete="チーム情報"
-                    label="チーム情報"
-                    height="200"
-                  />
-                </v-col>
-              </v-row>
-            </v-container>
-          </div>
-        </div>
-        <div class="page-content-item-footer">
-          <common-button @click="goTeamDetail" button-color="primary">
-            口コミ情報（最新１件）
-          </common-button>
-        </div>
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item>
+            <div class="page-content-item-main">
+              <div class="page-content-item-list">
+                <v-card class="d-inline-block mx-auto">
+                  <v-container>
+                    <v-row justify="space-between">
+                      <v-col cols="auto">
+                        <v-img
+                          height="200"
+                          width="200"
+                          src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card>
+              </div>
+              <div class="page-content-item-list">
+                <v-container>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <h1>チームトップ情報</h1>
+                      <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </div>
+            </div>
+            <div class="page-content-item-footer">
+              <common-button @click="goTeamDetail" button-color="primary">
+                口コミ情報（最新１件）
+              </common-button>
+            </div>
+          </v-tab-item>
+          <v-tab-item>
+            <div class="page-content-item-main">
+              <div class="page-content-item-list">
+                <v-container>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <h1>users.nameさんの口コミ</h1>
+                      <h2>性別[genders.men]年代[ages.category]在籍年[enrollments.yaer]あなたの立場[player_flags.player]</h2>
+                      <div class="d-flex justify-left align-center">
+                        <v-rating v-model="rating" />
+                        <div>{{ rating }}</div>
+                        <div>方針:5体制:5活動:5環境:5イベント:5費用:5</div>
+                      </div>
+                      <v-card class="d-inline-block mx-auto" min-width="60vw">
+                        <v-card-title>
+                          口コミ評価
+                        </v-card-title>
+                        <v-card-subtitle>
+                          評価理由
+                        </v-card-subtitle>
+                        <v-card-text>
+                          [総合]
+                          -general_post
+                        </v-card-text>
+                        <v-card-text>
+                          [チーム方針]
+                          -policy_post
+                        </v-card-text>
+                        <v-card-text>
+                          [チーム体制]
+                          - organization_post
+                        </v-card-text>
+                        <v-card-text>
+                          [活動内容]
+                          - activity_post
+                        </v-card-text>
+                        <v-card-text>
+                          [チーム環境]
+                          - environment_post
+                        </v-card-text>
+                        <v-card-text>
+                          [イベント]
+                          - event_post
+                        </v-card-text>
+                        <v-card-text>
+                          [費用]
+                          - cost_post
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </div>
+            </div>
+          </v-tab-item>
+          <v-tab-item>
+            <div class="page-content-item-main">
+              <div class="page-content-item-list">
+                <v-container>
+                  <v-row justify="space-between">
+                    <v-col cols="auto">
+                      <h1>teamの活動場所</h1>
+                      <h2>性別[genders.men]年代[ages.category]在籍年[enrollments.yaer]あなたの立場[player_flags.player]</h2>
+                      <div class="d-flex justify-left align-center">
+                        <v-rating v-model="rating" />
+                        <div>{{ rating }}</div>
+                        <div>方針:5体制:5活動:5環境:5イベント:5費用:5</div>
+                      </div>
+                      <v-card class="d-inline-block mx-auto" min-width="60vw">
+                        <iframe
+                          src="http://maps.google.co.jp/maps?q=〒131-0031 東京都墨田区墨田４丁目６−５&output=embed&t=m&z=16&hl=ja"
+                          frameborder="0"
+                          scrolling="no"
+                          marginheight="0"
+                          marginwidth="0"
+                          width="100%"
+                          height="450"
+                        />
+                        <v-card-title>〒131-0031 東京都墨田区墨田４丁目６−５</v-card-title>
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </div>
+            </div>
+          </v-tab-item>
+        </v-tabs-items>
       </div>
       <v-divider :inset="false" />
     </v-flex>
@@ -106,6 +194,8 @@ export default {
   data () {
     return {
       colors,
+      tab: null,
+      rating: 3,
       valid: true,
       nickname: '',
       email: '',
