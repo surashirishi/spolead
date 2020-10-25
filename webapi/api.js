f7f9559ab79a75e10d91a537345914a47dac129e
satoshi <urano3104@gmail.com>
2020-10-25 17:05:35 +0900

    branch develop

--- webapi/api.js
+++ webapi/api.js
@@ -1,8 +1,8 @@
 // localVer
-// const foundation = 'http://localhost:3000/v1'
+const foundation = 'http://localhost:3000/v1'
 
 // productionVer
-const foundation = process.env.FOUNDATION
+// const foundation = process.env.FOUNDATION
 
 export default {
   // 都道府県取得外部API
